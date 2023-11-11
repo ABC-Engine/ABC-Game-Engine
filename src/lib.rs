dd821c3415c96e4ece5f1e87cfc5ff9bf8be6b4b
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-11-10 19:04:41 -0600

    Added alpha support for rectangles
    I forgot to add for rectangles as well as circles

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,7 @@ pub struct Renderer {
     height: u32,
     pub objects: Vec<Object>,
     stretch: f32,
+    pub background_color: Color,
 }
 
 pub fn new_renderer(width: u32, height: u32) -> Renderer {
@@ -42,23 +43,19 @@ pub fn new_renderer(width: u32, height: u32) -> Renderer {
         height,
         objects: Vec::new(),
         stretch: 2.3,
+        background_color: Color {
+            r: 0,
+            g: 0,
+            b: 100,
+            a: 1.0,
+        },
     }
 }
 
 impl Renderer {
     pub fn render(&self) {
-        let mut pixel_grid = vec![
-            vec![
-                Color {
-                    r: 0,
-                    g: 0,
-                    b: 0,
-                    a: 0.0
-                };
-                self.width as usize
-            ];
-            self.height as usize
-        ];
+        let mut pixel_grid =
+            vec![vec![self.background_color; self.width as usize]; self.height as usize];
         for object in &self.objects {
             // check if object is circle or rectangle
             match object {
@@ -84,7 +81,12 @@ impl Renderer {
         let mut screen_string = String::new();
         for row in pixel_grid {
             for pixel in row {
-                screen_string.push_str(&format!("{}", "=".truecolor(pixel.r, pixel.g, pixel.b)));
+                if pixel.a == 0.0 {
+                    screen_string.push_str(&format!("{}", " "));
+                } else {
+                    screen_string
+                        .push_str(&format!("{}", "=".truecolor(pixel.r, pixel.g, pixel.b)));
+                }
             }
             screen_string.push_str("\n");
         }
@@ -106,11 +108,10 @@ pub fn render_circle(circle: &Circle, pixel_grid: &mut Vec<Vec<Color>>, stretch:
             let dy = y as f64 - circle.y;
             let distance_squared = dx.powi(2) + dy.powi(2);
             if distance_squared <= squared_radius {
-                //*pixel = circle.color;
-                // alpha
                 if circle.color.a == 1.0 {
-                    *pixel = circle.color; // easier and should capture division by 0
+                    *pixel = circle.color;
                 } else {
+                    // there has to be a better way to do this but I'm not sure how...
                     pixel.r = (pixel.r as f32 * (1.0 - circle.color.a) as f32) as u8;
                     pixel.r += (circle.color.r as f32 * circle.color.a) as u8;
                     pixel.g = (pixel.g as f32 * (1.0 - circle.color.a) as f32) as u8;
@@ -125,6 +126,9 @@ pub fn render_circle(circle: &Circle, pixel_grid: &mut Vec<Vec<Color>>, stretch:
 }
 
 pub fn render_rectangle(rectangle: &Rectangle, pixel_grid: &mut Vec<Vec<Color>>, stretch: &f32) {
+    if rectangle.color.a == 0.0 {
+        return;
+    }
     for x in 0..pixel_grid[0].len() {
         for y in 0..pixel_grid.len() {
             let pixel = &mut pixel_grid[y][x];
@@ -134,7 +138,17 @@ pub fn render_rectangle(rectangle: &Rectangle, pixel_grid: &mut Vec<Vec<Color>>,
                 && y as f64 >= rectangle.y
                 && y as f64 <= rectangle.y + rectangle.height
             {
-                *pixel = rectangle.color;
+                if rectangle.color.a == 1.0 {
+                    *pixel = rectangle.color;
+                } else {
+                    pixel.r = (pixel.r as f32 * (1.0 - rectangle.color.a) as f32) as u8;
+                    pixel.r += (rectangle.color.r as f32 * rectangle.color.a) as u8;
+                    pixel.g = (pixel.g as f32 * (1.0 - rectangle.color.a) as f32) as u8;
+                    pixel.g += (rectangle.color.g as f32 * rectangle.color.a) as u8;
+                    pixel.b = (pixel.b as f32 * (1.0 - rectangle.color.a) as f32) as u8;
+                    pixel.b += (rectangle.color.b as f32 * rectangle.color.a) as u8;
+                    pixel.a = 1.0;
+                }
             }
         }
     }
