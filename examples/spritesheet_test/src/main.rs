72f9445522e5c09fd67aba9e6cbd723f116f8c16
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2024-01-02 11:19:54 -0600

    Added support for spritesheets with multiple rows
    Improved the example to reflect this.

--- examples/spritesheet_test/src/main.rs
+++ examples/spritesheet_test/src/main.rs
@@ -1,30 +1,130 @@
 // this highlights some major issues with the current renderer
 use ABC_Game_Engine::*;
 
-const WINDOW_DIMS: (u32, u32) = (80, 80);
+const WINDOW_DIMS: (u32, u32) = (160, 160);
+
+struct MovementSystem {
+    player: Entity,
+    idle_animations: Vec<Animation>,
+    walk_animations: Vec<Animation>,
+    /// 0 = up_index, 1 = right_index, 2 = down_index, 3 = left_index
+    /// this is used to prevent the animation from changing if the player is already facing that direction
+    direction: u8,
+    is_idle: bool,
+}
+
+impl System for MovementSystem {
+    fn run(&mut self, entities_and_components: &mut EntitiesAndComponents) {
+        const up_index: u8 = 0;
+        const right_index: u8 = 2;
+        const down_index: u8 = 3;
+        const left_index: u8 = 1;
+
+        let mut normalized_dir = [0.0 as f64; 2];
+        let delta_time: f64;
+        {
+            delta_time = entities_and_components
+                .get_resource::<DeltaTime>()
+                .expect("failed to get delta time")
+                .delta_time;
+            let input = entities_and_components
+                .get_resource::<Input>()
+                .expect("failed to get input");
+            if input.is_key_pressed(Vk::W) {
+                normalized_dir[1] += -1.0;
+            }
+            if input.is_key_pressed(Vk::S) {
+                normalized_dir[1] += 1.0;
+            }
+            if input.is_key_pressed(Vk::A) {
+                normalized_dir[0] += -1.0;
+            }
+            if input.is_key_pressed(Vk::D) {
+                normalized_dir[0] += 1.0;
+            }
+            let magnitude = (normalized_dir[0].powi(2) + normalized_dir[1].powi(2)).sqrt();
+            if magnitude != 0.0 {
+                normalized_dir[0] /= magnitude;
+                normalized_dir[1] /= magnitude;
+            }
+        }
+
+        let (transform, sprite) =
+            entities_and_components.get_components_mut::<(Transform, Sprite)>(self.player);
+
+        transform.x += normalized_dir[0] * 10.0 * delta_time;
+        transform.y += normalized_dir[1] * 10.0 * delta_time;
+
+        let mut animation = match sprite {
+            Sprite::Animation(animation) => animation,
+            _ => panic!("Player sprite is not an animation"),
+        };
+
+        if normalized_dir[0] == 0.0 && normalized_dir[1] == 0.0 {
+            if self.is_idle {
+                return;
+            }
+            self.is_idle = true;
+            *animation = self.idle_animations[self.direction.min(3) as usize].clone();
+        } else if normalized_dir[0] > 0.0 {
+            self.is_idle = false;
+
+            if self.direction == right_index {
+                return;
+            }
+            *animation = self.walk_animations[right_index as usize].clone();
+            self.direction = right_index;
+        } else if normalized_dir[0] < 0.0 {
+            self.is_idle = false;
+
+            if self.direction == left_index {
+                return;
+            }
+            *animation = self.walk_animations[left_index as usize].clone();
+            self.direction = left_index;
+        } else if normalized_dir[1] > 0.0 {
+            self.is_idle = false;
+
+            if self.direction == up_index {
+                return;
+            }
+            *animation = self.walk_animations[up_index as usize].clone();
+            self.direction = up_index;
+        } else if normalized_dir[1] < 0.0 {
+            self.is_idle = false;
+
+            if self.direction == down_index {
+                return;
+            }
+            *animation = self.walk_animations[down_index as usize].clone();
+            self.direction = down_index;
+        }
+    }
+}
 
 // Note: this does not work in vscode terminal, but it does work in the windows terminal
 fn main() {
     let mut renderer = Renderer::new(WINDOW_DIMS.0, WINDOW_DIMS.1);
     renderer.set_stretch(1.0);
     let mut scene = Scene::new();
+    let player_entity: Entity;
     {
         let entities_and_components = &mut scene.game_engine.entities_and_components;
 
         scene.scene_params.set_background_color(Color {
-            r: 255,
-            g: 255,
-            b: 255,
+            r: 125,
+            g: 125,
+            b: 125,
             a: 1.0,
         });
 
         scene.scene_params.set_random_chars(false);
         scene.scene_params.set_character('█');
 
-        let animation = load_spritesheet(4, 100, "sprite_sheet_golem_0_16x16.png");
+        let idle_animations = load_spritesheet(4, 4, 100, "Animations/sprite_sheet_idle.png");
 
-        entities_and_components.add_entity_with((
-            Sprite::Animation(animation),
+        player_entity = entities_and_components.add_entity_with((
+            Sprite::Animation(idle_animations[0].clone()),
             Transform {
                 x: 40.0,
                 y: 20.0,
@@ -34,6 +134,19 @@ fn main() {
                 origin_y: 0.0,
             },
         ));
+    }
+
+    {
+        let idle_animations = load_spritesheet(4, 4, 100, "Animations/sprite_sheet_idle.png");
+        let walk_animations = load_spritesheet(4, 4, 100, "Animations/sprite_sheet_walk.png");
+
+        scene.game_engine.add_system(Box::new(MovementSystem {
+            player: player_entity,
+            idle_animations,
+            walk_animations,
+            direction: 0,
+            is_idle: true,
+        }));
     }
 
     loop {
--- src/load_texture.rs
+++ src/load_texture.rs
@@ -41,46 +41,60 @@ pub fn load_texture(path: &str) -> Texture {
     return new_texture;
 }
 
-pub fn load_spritesheet(number_of_cells: u32, frame_duration_ms: u64, path: &str) -> Animation {
-    let mut spritesheet = Animation {
-        frames: vec![],
-        current_frame: 0,
-        frame_time: Duration::from_millis(frame_duration_ms),
-        current_frame_start_time: Instant::now(),
-        loop_animation: true,
-        finished: false,
-    };
-
+/// Loads a spritesheet from a file
+/// The spritesheet must be a grid of cells
+/// Returns a vector of animations, one for each row of cells
+pub fn load_spritesheet(
+    number_of_cells_horizontal: u32,
+    number_of_cells_vertical: u32,
+    frame_duration_ms: u64,
+    path: &str,
+) -> Vec<Animation> {
+    let mut spritesheets = vec![];
     let texture = load_texture(path);
     // split the texture into frames
     let (texture_width, texture_height) = (texture.pixels[0].len(), texture.pixels.len());
-    let cell_width = texture_width as u32 / number_of_cells;
-    let cell_height = texture_height as u32;
-    for i in 0..number_of_cells {
-        let mut new_texture = Texture {
-            pixels: vec![
-                vec![
-                    Color {
-                        r: 0,
-                        g: 0,
-                        b: 0,
-                        a: 0.0,
-                    };
-                    cell_width as usize
-                ];
-                cell_height as usize
-            ],
+    let cell_width = texture_width as u32 / number_of_cells_horizontal;
+    let cell_height = texture_height as u32 / number_of_cells_vertical;
+
+    for vertical_cell_index in 0..number_of_cells_vertical {
+        let mut spritesheet = Animation {
+            frames: vec![],
+            current_frame: 0,
+            frame_time: Duration::from_millis(frame_duration_ms),
+            current_frame_start_time: Instant::now(),
+            loop_animation: true,
+            finished: false,
         };
-        for x in 0..cell_width {
-            for y in 0..cell_height {
-                let pixel = texture.pixels[y as usize][(x + (i * cell_width)) as usize];
-                new_texture.pixels[y as usize][x as usize] = pixel;
+
+        for horizonal_cell_index in 0..number_of_cells_horizontal {
+            let mut new_texture = Texture {
+                pixels: vec![
+                    vec![
+                        Color {
+                            r: 0,
+                            g: 0,
+                            b: 0,
+                            a: 0.0,
+                        };
+                        cell_width as usize
+                    ];
+                    cell_height as usize
+                ],
+            };
+            for x in 0..cell_width {
+                for y in 0..cell_height {
+                    let pixel = texture.pixels[(y + (vertical_cell_index * cell_height)) as usize]
+                        [(x + (horizonal_cell_index * cell_width)) as usize];
+                    new_texture.pixels[y as usize][x as usize] = pixel;
+                }
             }
+            spritesheet.frames.push(Image {
+                texture: new_texture,
+            });
         }
-        spritesheet.frames.push(Image {
-            texture: new_texture,
-        });
+        spritesheets.push(spritesheet);
     }
 
-    return spritesheet;
+    return spritesheets;
 }
