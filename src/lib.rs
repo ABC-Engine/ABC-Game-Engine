8c8214f07aceb172b56d10fd1c5dce9cc248cc73
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2024-04-23 10:39:42 -0500

    Completely remove console_renderer
    Moved to a different repo, this breaks examples temporarily

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,12 @@
 //#![doc = include_str!("../README.md")]
 
 mod resources;
-mod shape_renderer;
 mod test;
-use shape_renderer::*;
-mod load_texture;
 pub use crossterm::event::KeyCode;
-pub use load_texture::*;
-use renderer::*;
 pub use resources::*;
 pub use resources::{delta_time, input};
 pub use ABC_ECS::{Component, EntitiesAndComponents, Entity, System, World};
-pub mod camera;
 pub mod physics;
-pub mod renderer;
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: f32,
-}
-
-impl Default for Color {
-    fn default() -> Self {
-        Color {
-            r: 0,
-            g: 0,
-            b: 0,
-            a: 1.0,
-        }
-    }
-}
 
 /// Transform is a struct that holds the position, rotation, and scale of an object
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -92,62 +66,6 @@ impl<'a, 'b> std::ops::Add<&'b Transform> for &'a Transform {
             origin_x: self.origin_x - other.x as f32,
             origin_y: self.origin_y - other.y as f32,
         }
-    }
-}
-
-/// SceneParams is a struct that holds the background color, if the characters are random, and the character that will be displayed otherwise
-#[derive(Clone, Copy)]
-pub struct SceneParams {
-    background_color: Color,
-    is_random_chars: bool,
-    character: char,
-}
-
-impl SceneParams {
-    pub fn new() -> SceneParams {
-        SceneParams {
-            background_color: Color {
-                r: 0,
-                g: 0,
-                b: 0,
-                a: 1.0,
-            },
-            is_random_chars: false,
-            character: '=',
-        }
-    }
-
-    /// the background color of the scene
-    pub fn set_background_color(&mut self, color: Color) {
-        self.background_color = color;
-    }
-
-    /// the background color of the scene
-    pub fn with_background_color(mut self, color: Color) -> Self {
-        self.set_background_color(color);
-        self
-    }
-
-    /// if the characters are random, or if they are all the same character as specified by the set_character function
-    pub fn set_random_chars(&mut self, is_random_chars: bool) {
-        self.is_random_chars = is_random_chars;
-    }
-
-    /// if the characters are random, or if they are all the same character as specified by the set_character function
-    pub fn with_random_chars(mut self, is_random_chars: bool) -> Self {
-        self.set_random_chars(is_random_chars);
-        self
-    }
-
-    /// the character that will be displayed if is_random_chars is false
-    pub fn set_character(&mut self, character: char) {
-        self.character = character;
-    }
-
-    /// the character that will be displayed if is_random_chars is false
-    pub fn with_character(mut self, character: char) -> Self {
-        self.set_character(character);
-        self
     }
 }
 
